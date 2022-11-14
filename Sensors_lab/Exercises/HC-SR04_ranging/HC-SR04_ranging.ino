596af80fddf8c63948b23c01aefa3d91a8f31ddb
Adam-Fabo <107214595+Adam-Fabo@users.noreply.github.com>
2022-11-14 11:18:01 +0100

    Add files via upload

---
+++ Sensors_lab/Exercises/HC-SR04_ranging/HC-SR04_ranging.ino
@@ -0,0 +1,74 @@
+// todo top comment
+
+#define echoPin 2 // Attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 // Attach pin D3 Arduino to pin Trig of HC-SR04
+
+long duration;   // Variable for the duration of sound wave travel
+int distance;    // Variable for the distance measurement
+
+double speed_of_sound = 0.034;
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an INPUT
+  
+  Serial.begin(9600);       // Start serial communication 
+  Serial.println("Started serial communication"); 
+}
+
+
+void loop() {
+
+  // Make pulse of lenght 10 microseconds as per documentation
+  // https://cdn.sparkfun.com/datasheets/Sensors/Proximity/HCSR04.pdf
+
+  /**********************************************
+
+  Your assignment is to get measure distance using HC-SR04 ultrasonic distance sensor
+
+  1. Start ranging
+    - Make pulse of length 10 microseconds using functions digitalWrite() and delayMicroseconds()
+
+  2. Get the response
+    - Use function pulseIn(echoPin, HIGH) and store the result in durtation variable
+
+  Now you have stored the time it took to travel sound from sonar to obstacle and back.
+  In order to get result in meaningfull units (like centimeters) you neeed to transform duration into distance
+
+  3. Calculate distance
+    - Divide the duration with speed of sound. Dont forget to divide the result by 2 (soundwave travels twice the distance)
+    - Result will be in cm
+  
+  See the Serial monitor/plotter for results.
+
+
+  Write your code bellow this line
+  **********************************************/
+  
+
+
+
+
+
+
+
+  // Do not edit code under this line
+  /******************************************************************/
+
+  // slow down the code little bit
+  delay(100);
+
+  // Pretty print the results using Serial communication
+  Serial.print("Distance:");
+  Serial.print(distance);
+  Serial.println("cm");
+}
+
+
+
+
+
+
+
+
+
---
+++ Sensors_lab/Exercises/IMU_ang_rate_integral_solved/IMU_ang_rate_integral_solved.ino
@@ -0,0 +1,157 @@
+// todo top comment
+
+
+#include <Wire.h>
+
+// Gyroscope address
+#define IMU_gyroscope_address 0x6B
+
+// Control register where setup value is written
+#define Gyroscope_control_reg 0x11
+
+// Value that says how fast should be the measurements taken and what is the range of gyro
+#define Gyroscope_setup_value 0x7C
+
+// Addresses of register where output values are stored
+#define Ang_Rate_X_Low  0x22
+#define Ang_Rate_X_High 0x23
+#define Ang_Rate_Y_Low  0x24
+#define Ang_Rate_Y_High 0x25
+#define Ang_Rate_Z_Low  0x26
+#define Ang_Rate_Z_High 0x27
+
+
+// Variables where output acceleration values will be stored
+int ang_x = 0;
+int ang_y = 0;
+int ang_z = 0;
+
+// Time data needed for the integration
+unsigned long prev_time = 0;
+unsigned long now_time = 0;
+unsigned long time_diff = 0;
+
+
+// Variables where intregrated angular rate will be stored
+double ang_x_integral = 0;
+double ang_y_integral = 0;
+double ang_z_integral = 0;
+
+// Do not edit this function
+void setup() {
+  // Join I2C bus
+  Wire.begin();  
+
+  // Startup serial communication              
+  Serial.begin(9600);
+
+  // Setup the IMU  
+  Wire.beginTransmission(IMU_gyroscope_address); 
+  Wire.write(Gyroscope_control_reg);      
+  Wire.write(Gyroscope_setup_value);      
+  Wire.endTransmission();                        
+
+}
+
+
+void loop() {
+
+
+  // Read raw data from IMU, you need to implement this function down below
+  ang_x = get_angular_rate(Ang_Rate_X_Low, Ang_Rate_X_High);
+  ang_y = get_angular_rate(Ang_Rate_Y_Low, Ang_Rate_Y_High);
+  ang_z = get_angular_rate(Ang_Rate_Z_Low, Ang_Rate_Z_High);
+
+
+  // Converts raw data from IMU into unit of degrees per second
+  ang_x = ang_2_dps(ang_x);
+  ang_y = ang_2_dps(ang_y);
+  ang_z = ang_2_dps(ang_z);
+
+    /**********************************************
+
+  Your assignment is to get Integrate the data read from IMU - which says about change in degrees per second
+  and integrate it into relative cumulative angle
+
+  Here are steps how to do it:
+
+  1.  - You need to know time that the measurement was taken
+      - You need to know time of previous measurement taken
+      - You need to know the difference between those times
+      - Scale the difference into seconds
+      - Calulate the integral
+
+  Use function millis() to get current time (returns number of millisecond that passed from start of the program)   
+
+  Write your code bellow this line
+  **********************************************/
+
+
+  now_time = millis();
+  time_diff = now_time - prev_time;
+  prev_time = now_time;
+
+
+  ang_x_integral += ang_x * (time_diff/1000.0);
+  ang_y_integral += ang_y * (time_diff/1000.0);
+  ang_z_integral += ang_z * (time_diff/1000.0);
+
+
+  // Do not edit code under this line
+  /******************************************************************/
+
+  // Pretty print the results using Serial communication
+  Serial.print("Angle_X:"); Serial.print(ang_x_integral); Serial.print(" , ");
+  Serial.print("Angle_Y:"); Serial.print(ang_y_integral); Serial.print(" , ");
+  Serial.print("Angle_Z:"); Serial.println(ang_z_integral);
+
+}
+
+
+// Function that reads 2 bytes of angular rate and returns it as single byte 
+int get_angular_rate(byte low_address, byte high_address){
+
+  int lower_value  = 0;
+  int higher_value = 0;
+  int final_value  = 0;
+
+
+  Wire.beginTransmission(IMU_gyroscope_address);  // Start transmission to IMU
+  Wire.write(low_address);                        // Select register from which to read
+  Wire.endTransmission();                         // End transmission to IMU 
+
+  Wire.requestFrom(IMU_gyroscope_address, 2);     // Request 2 bytes from IMU
+
+  if (2 <= Wire.available()){                     // Read 2 bytes from IMU
+    lower_value = Wire.read(); 
+    higher_value = Wire.read();
+  } 
+
+  
+  // Combine those two values
+  final_value = higher_value * 256;
+  final_value += lower_value;
+
+  return final_value;
+
+}
+
+
+
+// Converts raw value from MinIMU into degrees per second
+double ang_2_dps(int ang_rate){
+  
+  // Scale down the value that is in range <-32,768, +32,768> into range <-2000, 2000>
+
+  return map(ang_rate, -32768, 32768, -2000, 2000);;
+
+}
+
+
+
+
+
+
+
+
+
